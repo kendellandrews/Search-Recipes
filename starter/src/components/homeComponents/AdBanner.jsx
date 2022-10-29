4e640a5ac4889f55e31f9026d02f2b408e410c51
Kendell Andrews <kendellandrews2@gmail.com>
2022-10-29 13:59:34 -0700

    saturday

--- starter/src/components/homeComponents/AdBanner.jsx
+++ starter/src/components/homeComponents/AdBanner.jsx
@@ -22,7 +22,7 @@ const AdBanner = () => {
           brown sugar rub, baked for 25 minutes on a bed of pineapple, and garnished in butter, garlic, and chives. You won’t want to miss it!
         </h4>
         <Link to="/recipe/3">
-          <button className="blue-btn">Check it out</button>
+          <button className="blueBtn">Check it out</button>
         </Link>
       </div>
     </div>
--- starter/src/components/newRecipeComponents/RecipeCard.jsx
+++ starter/src/components/newRecipeComponents/RecipeCard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-// import styles from "./RecipeCard.module.css";
 import { useNavigate } from "react-router-dom";
 
 const RecipeCard = ({recipe}) => {
@@ -13,11 +12,11 @@ const RecipeCard = ({recipe}) => {
         <div className="recipe-div">
             <div>
                 <div className="recipe-card">
-                    <img src="/Users/kendellandrews/specalization/react-proj-3/starter/src/assets/salad.webp" alt="" />
+                    <img id="imgSld" src="src/assets/salad.webp" alt="" />
                 </div>
                 <h3>{recipe.recipe_name}</h3>
+            <button className="seeMoreBtn" onClick={handleClick}>See More</button>
             </div>
-            <button className="blue-btn" onClick={handleClick}>See More</button>
         </div>
     );
 };
